02547ae33dd5ce63552c671dd04e02ecdd09f887
Fredy <fredywhatley@gmail.com>
2016-08-03 11:37:46 -0400

    add CRUD operations from http://www.asp.net/web-api/overview/older-versions/creating-a-web-api-that-supports-crud-operations

---
+++ ProductStore/Areas/HelpPage/SampleGeneration/SampleDirection.cs
@@ -0,0 +1,11 @@
+namespace ASP_WebApi_CRUD.Areas.HelpPage
+{
+    /// <summary>
+    /// Indicates whether the sample is used for request or response
+    /// </summary>
+    public enum SampleDirection
+    {
+        Request = 0,
+        Response
+    }
+}
---
+++ ProductStore/Models/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace ProductStore.Models
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetAll();
+        Product Get(int id);
+        Product Add(Product item);
+        void Remove(int id);
+        bool Update(Product item);
+    }
+}
